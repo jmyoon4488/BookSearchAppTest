7a9edf61e8d82351dbc3d8e4bbab6912963c3bcb
YJM <jmyoon4488@gmail.com>
2021-08-08 22:23:41 +0900

    [fix] 수정

--- app/src/main/java/com/jm4488/booksearch/network/NetworkParams.kt
+++ app/src/main/java/com/jm4488/booksearch/network/NetworkParams.kt
@@ -3,7 +3,7 @@ package com.jm4488.booksearch.network
 class NetworkParams {
 
     companion object {
-        val BASE_URL = "https://api.itbook.store"
+        const val BASE_URL = "https://api.itbook.store"
     }
 
     val testApiUrl = "https://api.itbook.store/1.0/search/android/1"
